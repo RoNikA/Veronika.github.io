7856f71c68952a0c8ab8500ce7017af15d6ec4a6
RoNikA <veronika-nikolaevna@mail.ru>
2016-04-28 15:45:49 +0300

    update 7-8

--- JS/jQ7_8/js/script.js
+++ JS/jQ7_8/js/script.js
@@ -1,6 +1,5 @@
 $(function(){
 
-
 var $tabs_1 = $('.tabs_1');
 var $t1 = $('.t1');
 var $tabs_2 = $('.tabs_2');
@@ -8,9 +7,7 @@ var $t2 = $('.t2');
 var $tabs_3 = $('.tabs_3');
 var $t3 = $('.t3');
 
-
-
-	$tabs_1.on('click', function() {
+$tabs_1.on('click', function() {
     $tabs_1.attr('id', 'active');
     $t1.attr('id', 'active');
     $tabs_2.removeAttr('id', 'active');
@@ -21,7 +18,7 @@ var $t3 = $('.t3');
 
 
 $tabs_2.on('click', function() {
-	$tabs_1.removeAttr('id', 'active');
+    $tabs_1.removeAttr('id', 'active');
     $t1.removeAttr('id', 'active');
     $tabs_2.attr('id', 'active');
     $t2.attr('id', 'active');
@@ -30,71 +27,75 @@ $tabs_2.on('click', function() {
 });
 
 $tabs_3.on('click', function() {
-	$tabs_1.removeAttr('id', 'active');
+    $tabs_1.removeAttr('id', 'active');
     $t1.removeAttr('id', 'active');
     $tabs_2.removeAttr('id', 'active');
     $t2.removeAttr('id', 'active');
     $tabs_3.attr('id', 'active');
     $t3.attr('id', 'active');
+   });
 });
 
- });
 
-
-
-var $firstName = $('.firstname');
-var $lastName = $('.lastname');
-var $adress = $('.address');
+var $firstName = $('.input-1');
+var $lastName = $('.input-2');
+var $adress = $('.input-3');
 var $button = $('button');
-var $helpText = $(".help_text");
 
-
-$firstName.hover(function(){$('title').css({
+$firstName.hover(function(){$('.appear-text1').css({
     opacity: 1,
     visibility:'visible'
 })}, 
 
-function() {$('title').css({
-    opacity: 0,
-    visibility:'hidden'
-})});
 
-$lastName.hover(function(){$('title').css({
-    opacity: 1,
-    visibility:'visible'
-})}, 
-
-function() {$('title').css({
-    opacity: 0,
-    visibility:'hidden'
-})});
-
-$adress.hover(function(){$('title').css({
-    opacity: 1,
-    visibility:'visible'
-})}, 
-
-function() {$('title').css({
+function() {$('.appear-text1').css({
     opacity: 0,
     visibility:'hidden'
 })});
 
 
+$lastName.hover(function(){$('.appear-text2').css({
+    opacity: 1,
+    visibility:'visible'
+})}, 
 
-$('button').click(function () {
-    
 
-                       if ($(".help_text").is(":hidden")) {
+function() {$('.appear-text2').css({
+    opacity: 0,
+    visibility:'hidden'
+})});
 
-                               $(".help_text").show("slow");
 
-                       } else {
+$adress.hover(function(){$('.appear-text3').css({
+    opacity: 1,
+    visibility:'visible'
+})}, 
 
-                               $(".help_text").hide("slow");
 
-                       }
- return false;
+function() {$('.appear-text3').css({
+    opacity: 0,
+    visibility:'hidden'
+})});
+
+
+$('button').on('click', function () {
+
+  $('.appear-text1').css({
+      opacity: 1,
+      visibility:'visible'
+    });
+
+  $('.appear-text2').css({
+      opacity: 1,
+      visibility:'visible'
+    });
+
+  $('.appear-text3').css({
+      opacity: 1,
+      visibility:'visible'   
+    });
 });
+
 
    
   
